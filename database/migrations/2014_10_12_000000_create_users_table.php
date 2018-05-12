4b4227bbc4d02c42426ca3182a1613e60c0077fb
danxmc <danx_97@hotmail.com>
2018-05-12 15:26:00 -0500

    Add description field to User model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('role');
+            $table->string('description');
             //$table->unsignedInteger('room_id')->nullable();
             $table->timestamps();
         });
