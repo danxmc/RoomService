d801590b436c55ffc1efed3e9689d3e6078d10c4
danxmc <danx_97@hotmail.com>
2018-05-12 19:17:49 -0500

    Fix description not nullable on User model, fix Room Seeder and Meal Factory

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
             'password' => '$2y$10$bfRtQPxybFNRkl7c8wnKYOAam0NbU8GnXwD9JEvj/LC8fOu4LRAwq',
             'remember_token' => str_random(10),
             'role' => 'ADMIN',
+            'description' => "Administrator",
         ]);
         User::create([
             'name' => 'Chef',
@@ -25,6 +26,7 @@ class UsersTableSeeder extends Seeder
             'password' => '$2y$10$48TlSzvAlgs9y/SzlQgXoe76gABYknT0AUGyA.fEowKcRv0gkgpgm',
             'remember_token' => str_random(10),
             'role' => 'COOK',
+            'description' => 'Graduated from IEST',
         ]);
         User::create([
             'name' => 'Client',
@@ -32,6 +34,7 @@ class UsersTableSeeder extends Seeder
             'password' => '$2y$10$45ejeGIbA.iZF12zoGzfKeh3RLeELEtS2wmCA0t98LyDdFUOdOn9e',
             'remember_token' => str_random(10),
             'role' => 'CLIENT',
+            'description' => 'NULL',
         ]);
         User::create([
             'name' => 'Client1',
@@ -39,6 +42,7 @@ class UsersTableSeeder extends Seeder
             'password' => '$2y$10$45ejeGIbA.iZF12zoGzfKeh3RLeELEtS2wmCA0t98LyDdFUOdOn9e',
             'remember_token' => str_random(10),
             'role' => 'CLIENT',
+            'description' => 'NULL',
         ]);
     }
 }
